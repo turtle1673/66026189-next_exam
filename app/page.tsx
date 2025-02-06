dd2fe7b99ca1de4fc0b57cf50e0982ea8218df24
turtle1673 <66026189@up.ac.th>
2025-02-06 14:39:02 +0700

    add studentID

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>66026189</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
